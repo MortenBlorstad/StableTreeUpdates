fb497d225eca1c40473e9f24bdb2034974e71841
Morten <mblorstad@gmail.com>
2024-01-22 11:55:29 +0100

    added result of main experiment on all datasets

--- StableTrees_examples/ISLR/ISLR.py
+++ StableTrees_examples/ISLR/ISLR.py
@@ -49,7 +49,7 @@ compute = False
 
 if compute:
     for ds,target in zip(datasets,targets ):
-        kf = RepeatedKFold(n_splits= 10,n_repeats=5, random_state=SEED)
+        kf = RepeatedKFold(n_splits= 5,n_repeats=10, random_state=SEED)
         stability = {c:[] for c in search_grid}
         mse = {c:[] for c in search_grid}
 
@@ -123,7 +123,7 @@ if compute:
 
     df_list = list(itertools.chain(*plot_info.values()))
     df = pd.DataFrame(df_list, columns=["dataset","method",'loss', 'stability', "alpha","beta", 'loss_abs', "stability_abs",'loss_se', 'stability_se', 'loss_abs_se', 'stability_abs_se'  ] )
-    df.to_csv('StableTrees_examples/results/main_experiment_ISLR_k=10_r=5.csv', index=False)
+    df.to_csv('StableTrees_examples/results/main_experiment_ISLR.csv', index=False)
 else:
     from matplotlib import pyplot as plt
     from adjustText import adjust_text
@@ -145,7 +145,7 @@ else:
     fig, axes = plt.subplots(nrows=2, ncols=3, figsize=( 11.7, 2*11.7/3), dpi=500)
     axes = axes.ravel()
     plt.rcParams.update(plot_params)
-    df =pd.read_csv('StableTrees_examples/results/main_experiment_ISLR_k=10_r=5.csv')
+    df =pd.read_csv('StableTrees_examples/results/main_experiment_ISLR.csv')
     datasets =["Boston", "Carseats","College", "Hitters", "Wage"]
     for ds,ax in zip(datasets,axes[:-1]):
         # if ds != "College":
@@ -195,7 +195,7 @@ else:
     # adjust spacing between subplots
     fig.tight_layout()
     #plt.show()
-    plt.savefig(f"StableTrees_examples\plots\main_experiment_ISLR_k=10_r=5.png")
+    plt.savefig(f"StableTrees_examples\plots\main_experiment_ISLR.png")
     plt.close()
 
 
